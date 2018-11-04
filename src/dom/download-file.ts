4a32bac9185d1cbecf5741f49fa3eeb1afa86dfd
Ash <ash.norseman@gmail.com>
2018-11-05 07:50:39 +0800

    bugfix

--- src/dom/download-file.ts
+++ src/dom/download-file.ts
@@ -2,19 +2,20 @@
  * Let the browser start downloading a file
  */
 
+const id = `FRAME_DOWNLOAD_${Date.now()}`;
+
 export function downloadFile(src: string) {
+  const oldFrame: HTMLIFrameElement = document.getElementById(id) as HTMLIFrameElement;
+
+  if (oldFrame) {
+    document.body.removeChild(oldFrame);
+  }
+
   const frame: HTMLIFrameElement = document.createElement('iframe');
 
+  frame.id = id;
   frame.style.display = 'none';
   frame.src = src;
-
-  frame.addEventListener(
-    'load',
-    () => {
-      document.body.removeChild(frame);
-    },
-    false,
-  );
 
   document.body.appendChild(frame);
 }
